ce9b5955838c5c06646a4f712a673bc7eb305620
navarrete2 <m4gu4l@icloud.com>
2024-03-15 17:42:41 +0100

    contentview customizado

--- ContentView.swift
+++ ContentView.swift
@@ -8,36 +8,48 @@ import SwiftUI
 import SDWebImageSwiftUI
 import FirebaseAnalytics
 import FirebaseAppCheck
+
 struct ContentView: View {
     @ObservedObject var productoViewModel = ProductoViewModel()
 
     var body: some View {
         NavigationView {
-            List(productoViewModel.productos) { producto in
-                NavigationLink(destination: DetalleProductoView(producto: producto)) {
-                    VStack(alignment: .leading) {
-                        Text(producto.nombre).font(.headline)
-                        Text(producto.descripcion)
-                        Text("\(producto.precio)")
-                        
-                        // Aquí es donde se muestra la imagen.
-                        if let url = URL(string: producto.imagen) {
-                            WebImage(url: url)
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                        } else {
-                            // Maneja el caso en que producto.imagen no es una URL válida.
-                            // Por ejemplo, podrías mostrar una imagen predeterminada.
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 200))]) {
+                    ForEach(productoViewModel.productos) { producto in
+                        NavigationLink(destination: DetalleProductoView(producto: producto)) {
+                            VStack(alignment: .leading) {
+                                if let url = URL(string: producto.imagen) {
+                                    WebImage(url: url)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(height: 150)
+                                        .cornerRadius(10)
+                                        .shadow(radius: 10)
+                                } else {
+                                    // Maneja el caso en que producto.imagen no es una URL válida.
+                                    // Por ejemplo, podrías mostrar una imagen predeterminada.
+                                }
+                                
+                                Text(producto.nombre)
+                                    .font(.headline)
+                                    .foregroundColor(.primary)
+                                Text(producto.descripcion)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                Text("\(producto.precio)")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.primary)
+                            }
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 10)
                         }
                     }
-                    .onAppear {
-                        Analytics.logEvent("producto_visto", parameters: [
-                            "nombre": producto.nombre,
-                            "descripcion": producto.descripcion,
-                            "precio": "\(producto.precio)"
-                        ])
-                    }
                 }
+                .padding()
             }
             .onAppear() {
                 self.productoViewModel.fetchProductos()
